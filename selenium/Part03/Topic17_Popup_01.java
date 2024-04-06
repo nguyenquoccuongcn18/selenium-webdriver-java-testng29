f9efa08194fa3d88bc10b7788989a2b418d85843
nguyenquoccuongcn18 <nguyenquoccuongcn18@gmail.com>
2024-04-06 22:00:02 +0700

    part03_topic17

--- selenium/Part03/Topic17_Popup_01.java
+++ selenium/Part03/Topic17_Popup_01.java
@@ -23,21 +23,69 @@ public class Topic17_Popup_01 {
 
     }
     @Test
-    public void TC_01_Fixed_Popup_in_DOM() {
+    public void TC_01_Fixed_Popup_in_DOM_1() {
         driver.get("https://ngoaingu24h.vn/");
         driver.findElement(By.xpath("//button[@class='login_ icon-before']")).click();
 
         By LoginPopup = By.xpath("//div[@class='modal fade in']//div[@class='modal-dialog']//div[@class='modal-content']");
+        By LoginPopup1 = By.cssSelector("div[id='modal-login-v1'][style]>div");
         //Ktra login popup đang hiển thị
-        Assert.assertTrue(driver.findElement(LoginPopup).isDisplayed());
+        Assert.assertTrue(driver.findElement(LoginPopup1).isDisplayed());
 
         driver.findElement(By.cssSelector("div[id='modal-login-v1'][style] input#account-input")).sendKeys("automatìonc");
         driver.findElement(By.cssSelector("div[id='modal-login-v1'][style] input#password-input")).sendKeys("automatìonc");
         driver.findElement(By.cssSelector("div[id='modal-login-v1'][style] button.btn-login-v1")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//div[text()='Tài khoản không tồn tại!']")).getText(),"Tài khoản không tồn tại!");
+        driver.findElement(By.cssSelector("div[id='modal-login-v1'][style] button.close")).click();
+        Assert.assertFalse(driver.findElement(LoginPopup1).isDisplayed());
+
+
     }
 
     @Test
-    public void TC_02_Logo() {
+    public void TC_02_Fixed_Popup_Not_in_DOM() {
+        driver.get("https://tiki.vn/");
+        By ButtonLoginTiKi = By.cssSelector("div[data-view-id='header_header_account_container']");
+        By Login = By.cssSelector("div[class='styles__Left-sc-2hr4xa-1 iwneWf']");
+        By ClickEmail = By.cssSelector("p.login-with-email");
+        By ClickDangNhap = By.xpath("//button[text()='Đăng nhập']");
+
+        driver.findElement(ButtonLoginTiKi).click();
+        //ktra hiển thị
+        Assert.assertTrue(driver.findElement(Login).isDisplayed());
+
+        driver.findElement(ClickEmail).click();
+        driver.findElement(ClickDangNhap).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='error-mess' and text()='Email không được để trống']")).getText(),"Email không được để trống");
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='error-mess' and text()='Mật khẩu không được để trống']")).getText(),"Mật khẩu không được để trống");
+
+        driver.findElement(By.cssSelector("img.close-img")).click();
+
+
+
+
+
+    }
+
+    @Test
+    public void TC_02_Fixed_Popup_Not_in_DOM_2() {
+        driver.get("https://www.facebook.com/");
+
+        driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();
+        sleepInsecons(5);
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='mbs _52lq fsl fwb fcb']")).isDisplayed());
+        sleepInsecons(5);
+        driver.findElement(By.xpath("//img[@class='_8idr img']")).click();
+
+
+
+        // do không sleep sẽ lỗi vì chưa load được để verify tắt popup
+        //Nếu tắt popup sẽ mất element nên sẽ dùng assertEquals - .sizr(),0
+        sleepInsecons(5);
+        Assert.assertEquals(driver.findElements(By.xpath("//div[@class='mbs _52lq fsl fwb fcb']")).size(),0);
+
 
     }
 
